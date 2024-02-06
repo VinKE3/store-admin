a055a44544721789a73b6619a0a161ae4850198c
Kevin Muñoz <harquin09@gmail.com>
2024-02-06 16:15:33 -0500

    fin

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -2,8 +2,10 @@
 
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
+import { useState } from "react";
 
 import { cn } from "@/lib/utils";
+import { ArrowDown, AlignCenter } from "lucide-react";
 
 export function MainNav({
   className,
@@ -11,7 +13,7 @@ export function MainNav({
 }: React.HTMLAttributes<HTMLElement>) {
   const pathname = usePathname();
   const params = useParams();
-
+  const [openMobileMenu, setOpenMobileMenu] = useState(false);
   const routes = [
     {
       href: `/${params.storeId}`,
@@ -60,20 +62,35 @@ export function MainNav({
       className={cn("flex items-center space-x-4 lg:space-x-6", className)}
       {...props}
     >
-      {routes.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
-          className={cn(
-            "text-sm font-medium transition-colors hover:text-primary",
-            route.active
-              ? "text-black dark:text-white"
-              : "text-muted-foreground"
-          )}
-        >
-          {route.label}
-        </Link>
-      ))}
+      <AlignCenter
+        className="block text-3xl md:hidden cursor-pointer"
+        onClick={() => setOpenMobileMenu(!openMobileMenu)}
+      />
+      <div
+        className={`${
+          openMobileMenu
+            ? "block dark:bg-black mt-4 bg-slate-100 rounded-xl"
+            : "hidden"
+        } w-full md:block md:w-auto`}
+      >
+        <div className="flex flex-col p-4 mt-28 md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0">
+          {routes.map((route) => (
+            <Link
+              key={route.href}
+              href={route.href}
+              onClick={() => setOpenMobileMenu(false)}
+              className={cn(
+                "text-sm font-medium transition-colors hover:text-primary",
+                route.active
+                  ? "text-black dark:text-white"
+                  : "text-muted-foreground"
+              )}
+            >
+              {route.label}
+            </Link>
+          ))}
+        </div>
+      </div>
     </nav>
   );
 }
